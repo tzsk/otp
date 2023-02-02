358408dcc94f4a1381a2f6d58751f140277d75ea
tzsk <mailtokmahmed@gmail.com>
2023-02-02 19:41:23 +0530

    add: Type Hints

--- config/otp.php
+++ config/otp.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    /*
+    /**
      * This value indicates the number of digits to be generated
      */
     'digits' => 6,
 
-    /*
+    /**
      * This value indicates the number of minutes until one OTP will be valid
      */
     'expiry' => 10,
--- src/Facades/Otp.php
+++ src/Facades/Otp.php
@@ -5,6 +5,15 @@ namespace Tzsk\Otp\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static \Tzsk\Otp\Otp digits(int $digits)
+ * @method static \Tzsk\Otp\Otp expiry(int $digits)
+ * @method static string make(string $key)
+ * @method static string create(string $key)
+ * @method static string generate(string $key)
+ * @method static boolean match(mixed $otp, string $key)
+ * @method static boolean check(mixed $otp, string $key)
+ * @method static boolean verify(mixed $otp, string $key)
+ *
  * @see \Tzsk\Otp\Otp
  */
 class Otp extends Facade
